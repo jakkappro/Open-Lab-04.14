2cb59cab3e1b72fe69ce37a67ea9dfdd3ab4add0
jakub kapitulcin <kapitulcinjakub1@gmail.com>
2020-10-03 13:59:45 +0200

    Finished Frame printer

--- Open-Lab-04.14/FramePrinter.cs
+++ Open-Lab-04.14/FramePrinter.cs
@@ -6,7 +6,38 @@ namespace Open_Lab_04._14
     {
         public void Print(string[] strings)
         {
-            throw new NotImplementedException();
+            string sideFrame = "";
+            int longest = 0;
+
+            foreach (string word in strings)
+            {
+                if (longest < word.Length)
+                    longest = word.Length;
+            }
+
+            for (int i = 0; i < longest + 4; i++)
+            {
+                sideFrame += "*";
+            }
+
+            Console.WriteLine(sideFrame);
+
+            foreach(string word in strings)
+            {
+                if (word.Length < longest)
+                {
+                    string afterWord = "";
+                    for(int i = 0; i < longest - word.Length; i++)
+                    {
+                        afterWord += " ";
+                    }
+                    Console.WriteLine("* " + word + afterWord +" *");
+                }
+                else
+                    Console.WriteLine("* " + word + " *");
+            }
+
+            Console.WriteLine(sideFrame);
         }
     }
 }
